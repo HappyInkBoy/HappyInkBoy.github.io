5b559dbbc408e163871e64ec0b25fbbdd0819d32
HappyInkBoy <alex612613@gmail.com>
2025-01-18 11:36:51 -0500

    added parser progress

--- web/editorexpression.py
+++ web/editorexpression.py
@@ -2,26 +2,19 @@ from browser import document, html, window
 
 import editormodel, editormatrices, linearalgebra
 
+import re
+
 model = editormodel.model
 
 def onExpressionModelUpdate():
   expression = model["expression"]
-  if expression == "A=B+C":
-    B = model["matrices"]["B"]
-    C = model["matrices"]["C"]
+  parser(expression)
 
-    A = model["matrices"]["A"]
-    A["rows"] = B["rows"]
-    A["cols"] = B["cols"]
-    A["matrix"] = [[0 for row in range(A["rows"])] for col in range(A["cols"])]
-    for row in range(A["rows"]):
-      for col in range(A["cols"]):
-        A["matrix"][row][col] = B["matrix"][row][col] + C["matrix"][row][col]
-  if expression == "A=RREF(B)":
-    B = model["matrices"]["B"]["matrix"]
-    M1 = linearalgebra.Matrix.from_2d_list(B)
-    temp_matrix = linearalgebra.Op.reducedRowEchelonForm(M1)
-    model["matrices"]["A"]["matrix"] = temp_matrix.give_2d_list()
+#  if expression == "A=RREF(B)":
+#    B = model["matrices"]["B"]["matrix"]
+#    M1 = linearalgebra.Matrix.from_2d_list(B)
+#    temp_matrix = linearalgebra.Op.reducedRowEchelonForm(M1)
+#    model["matrices"]["A"]["matrix"] = temp_matrix.give_2d_list()
 
   editormatrices.onMatrixModelUpdate("A")
 
@@ -45,3 +38,42 @@ def initExpression(parentNode):
   input = html.INPUT(id="expression-input")
   parentNode <= input
   input.bind("keyup", expressionInputAction)
+
+
+# Parser below
+
+
+regular_expressions = {
+  "assignment_exp": "^([A-Z])=(.+)",
+  "dual_exp": "^([A-Z])([+-☉·⨯])([A-Z])"
+}
+
+def parser(expression):
+  expression = expression.replace(" ", "")
+  r = re.search(regular_expressions["assignment_exp"], expression)
+  if r and r.group(1) and r.group(2):
+    parserAssignmentExpression(r.group(1), r.group(2))
+  
+
+def parserAssignmentExpression(variable, implicitExpression):
+  print(variable)
+  print(implicitExpression)
+  # dual expression ?
+  r = re.search(regular_expressions["dual_exp"], implicitExpression)
+  if r and r.group(1) and r.group(2) and r.group(3):
+    result = parserDualExpression(r.group(1), r.group(2), r.group(3))
+    modelMatrixResult = model["matrices"][variable]
+    modelMatrixResult["matrix"] = result
+  # or single expression ?
+
+def parserDualExpression(leftVariable, dualOperator, rightVariable):
+  lm = model["matrices"][leftVariable]["matrix"]
+  leftMatrix = linearalgebra.Matrix.from_2d_list(lm)
+  rm = model["matrices"][rightVariable]["matrix"]
+  rightMatrix = linearalgebra.Matrix.from_2d_list(rm)
+  if dualOperator == "+":
+    result = leftMatrix + rightMatrix
+  elif dualOperator == "-":
+    result = leftMatrix - rightMatrix
+
+  return result.give_2d_list()
