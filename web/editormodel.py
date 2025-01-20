0f0581c885461ecfe45ec428887575c1bf729fe5
HappyInkBoy <alex612613@gmail.com>
2025-01-20 14:33:29 -0500

    Added more commands to the UI

--- web/editormodel.py
+++ web/editormodel.py
@@ -3,10 +3,14 @@ model = {
       { "command" : "+", "expression" : "+", "tooltip" : "Add 2 matrices. Example: A=B+C"},
       { "command" : "-", "expression" : "-",  "tooltip" : "Subtract 2 matrices. Example: A=B-C"},
       { "command" : "*", "expression" : "*",  "tooltip" : "Multiply 2 matrices. Example: A=B*C"},
+      { "command" : "^", "expression" : "^", "tooltip" : "Raise a matrix to the power of an exponent. Example: A=B^2"},
       { "command" : "⨯", "expression" : "⨯",  "tooltip" : "Cross product of 2 vectors. Example: A=B⨯C"},
       { "command" : "·", "expression" : "·",  "tooltip" : "Dot product of 2 vectors. Example: A=B·C"},
       { "command" : "☉", "expression" : "☉",  "tooltip" : "Hadamard product of 2 matrices. Example: A=B☉C"},
-      { "command" : "RREF", "expression" : "RREF()",  "tooltip" : "Reduce Row Echelon Form. Example: A=RREF(B)"}
+      { "command" : "RREF", "expression" : "RREF()",  "tooltip" : "Reduce Row Echelon Form. Example: A=RREF(B)"},
+      { "command" : "DET", "expression" : "DET()", "tooltip" : "Evaluates the determinant of a square matrix. Example: A=DET(B)"},
+      { "command" : "TRACE", "expression" : "TRACE()", "tooltip" : "Evaluates the trace of a square matrix. Example: A=TRACE(B)"},
+      { "command" : "TRANSPOSE", "expression" : "TRANSPOSE()", "tooltip" : "Swaps the rows and collumns of a matrix. Example: A=TRANSPOSE(B)"}
     ],
     "matrices" : {
         "A" : { "name" : "A", "rows" : 3, "cols" : 3, "matrix" : [[0,0,0],[0,0,0],[0,0,0]] },
