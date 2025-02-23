b13d264e9c9fac6f284de463dff532c3bf27b5a0
HappyInkBoy <alex612613@gmail.com>
2025-02-23 16:21:13 -0500

    Added base framework for recursive parser with exponentiation

--- web/editorexpression.py
+++ web/editorexpression.py
@@ -8,7 +8,8 @@ model = editormodel.model
 
 def onExpressionModelUpdate():
   expression = model["expression"]
-  parser(expression, False)
+  intermediateEquations = []
+  recursiveParser(expression, False, intermediateEquations, "ANSA")
 
 #  if expression == "A=RREF(B)":
 #    B = model["matrices"]["B"]["matrix"]
@@ -45,9 +46,95 @@ def initExpression(parentNode):
   input.bind("keyup", expressionInputAction)
 
 def isValidExpression(expression):
-  return parser(expression, True)
+  intermediateEquations = []
+  return recursiveParser(expression, True, intermediateEquations, "ANSA")
 
-# Parser below
+# Recursive Parser
+# 
+# Uses the Basic Parser for single and dual operators evaluation
+#
+# For complex expressions that the Basic Parser cannot directly handle:
+# - Uses intermediate ANSA ANSB ANSC... variables to progressively calculate the expression
+#   in the proper sequence wrt to parentheses and operation priority (^ over * over +-)
+#
+# Example 1: expression "rref(A+B+(C+D*A))"
+# rref(A+B+(C+D*A))
+# rref(A+B+(C+ANSA)) with ANSA=D*A expression sent to basic parser
+# rref(A+B+ANSB) with ANSB=C+ANSA
+# rref(ANSC+ANSB) with ANSC=A+B
+# rref(ANSD) wwith ANSD=ANSC+ANSB
+# and basic parser can handle this and assign to ANS
+#
+# Example 2: expression "A+B*D*E+F*G+I"
+#
+# A+B*D*E+F*G+I
+# A+ANSA*E+F*G+I with ANSA=B*D expression sent to basic parser 
+# A+ANSB+F*G+I with ANSB=ANSA*E
+# A+ANSB+ANSC+I with ANSC=F*G
+# ANSD+ANSC+I with ANSD=A+ANSB
+# ANSE+I with ANSE=ANSD+ANSC
+# and basic parser can handle this and assign to ANS
+#
+# Example 3: expression "(((VARIABLENAME)))"
+# (((VARIABLENAME)))
+# ((VARIABLENAME))
+# (VARIABLENAME)
+# VARIABLENAME
+# and basic parser can handle this and assign to ANS
+#
+# Example 4: expression "A^2+B^3*C^4"
+# A^2+B^3*C^4
+# ANSA+B^3*C^4 with ANSA=A^4 expression sent to basic parser
+# ANSA+ANSB*C^4 with ANSB=B^3
+# ANSA+ANSB*ANSC with ANSC=C^4
+# ANSA+ANSD with ANSD=ANSB*ANSC
+# and basic parser can handle this and assign to ANS
+#
+# Example 5: expression "A*(B+C)"
+# A*(B+C)
+# A*(ANSA) with ANSA=B+C
+# A*ANSA
+# and basic parser can handle this and assign to ANS
+
+recursive_regex = {
+  "exponent" : "([A-Z]+\^[-+]?\d*\.?\d+|\d+)"
+}
+
+def recursiveParser(expression, freeFlight, parentExpressions, nextAnsVariable):
+  result = parser(expression, freeFlight)
+  if result:
+    return result
+  # priorities
+  # regex for VARA^number - can be handled by basic parser
+  # regex for VARA*VARB and then other multiplications we support - can be handled by basic parser
+  # regex for ([any character except '(' and at least one non alpha character]+)
+  #   the above regex captures sub-expression within a proper set of parentheses. It can be recursively handled by recursiveParser
+  # regex for [beginning of expression or non alpha character]([alpha characters])
+  #   the above regex captures a set of parentheses that wraps up a variable name, not prefixing a single operator such as TRACE RREF etc.
+  #   it's intended to simply remove that unecessary set of parentheses - see Example 5. above
+  r = re.search(recursive_regex["exponent"], expression)
+  if r and r.group(1):
+    subExpression = f"{nextAnsVariable}={r.group(1)}"
+    result = parser(subExpression, freeFlight)
+    if not result:
+      return False
+    newExpression = expression[:r.span(1)[0]] + nextAnsVariable + expression[r.span(1)[1]:]
+    nextAnsVariable=nextAns(nextAnsVariable)
+    parentExpressions.append(expression)
+    return recursiveParser(newExpression, freeFlight, parentExpressions, nextAnsVariable)
+    # todo replace group(1) into expression with nextAnsVariable
+    # then call recursively ourselves with the new expression and the next nextAnsVariable
+  
+  return False
+
+# Example: A^2+B^2
+# ANSA+B^2
+
+def nextAns(variable):
+  last = variable[len(variable)-1]
+  nextLetter = chr(ord(last)+1)
+  return variable[:len(variable)-1] + nextLetter
+# Basic Parser below
 
 
 regular_expressions = {
