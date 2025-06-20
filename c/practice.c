25a16bc4a08db48d579925e822d60130c627e900
HappyInkBoy <61154335+HappyInkBoy@users.noreply.github.com>
2025-06-20 17:09:40 -0400

    Create practice.c

---
+++ c/practice.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* add(int *m1, int *m2) {
+  int *m3 = malloc(3 * 3 * sizeof(int));
+  int i;
+  for (i=0; i<9; i++) {
+    m3[i] = m1[i] + m2[i];
+  }
+  return m3;
+}
+
+void print(int *m) {
+  printf("%d %d %d\r\n", m[0], m[1], m[2]);
+  printf("%d %d %d\r\n", m[3], m[4], m[5]);
+  printf("%d %d %d\r\n", m[6], m[7], m[8]);
+}
+
+int main() {
+  printf("Ok");
+  int a=sizeof(int);
+  int *m1 = malloc(3 * 3 * sizeof(int));
+  int i;
+  int count = 0;
+  for (i=0; i < 3; i++) {
+    int j;
+    for (j=0; j < 3; j++) {
+      m1[3*j+i] = count;
+      count++;
+    }
+  }
+  print(m1);
+  int *m2 = malloc(3 * 3 * sizeof(int));
+  for (i=0; i < 3; i++) {
+    int j;
+    for (j=0; j < 3; j++) {
+      m2[3*j+i] = 10*count;
+      count++;
+    }
+  }
+  print(m2);
+
+  int* placeholder = add(m1,m2);
+  print(placeholder);
+
+  free(m1);
+  free(m2);
+  free(placeholder);
+  return 0;
+}
